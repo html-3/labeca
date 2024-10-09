f24653358e1cfbb6f18f11bf75f3fbf2a8bbb34d
Guilherme Matera <guimatera@gmail.com>
2024-10-09 10:16:25 -0300

    bugfix: Conserto dos valores de tau no bodeplotter e determinação do array de frequências a serem inseridas no experimento em bancada de resposta em frequência.

--- experimento2/frequencia/bode_plotter.m
+++ experimento2/frequencia/bode_plotter.m
@@ -35,7 +35,7 @@ function bode_plotter()
     ylabel('Fase (graus)');
     grid on;
 
-    % Plot de módulo e fase de Boda da F.T. do método neperiano
+    % Plot de módulo e fase de Bode da F.T. do método neperiano
     [mag_nep, phase_nep, w_nep] = bode(sys_neperiano);
     freq_nep = w_nep / (2*pi);
 
@@ -57,6 +57,13 @@ function bode_plotter()
 
     w_0 = 20*log10(K);
     
-    tau_gain = w_0-3
-    tau_num_area = 1/freq_area(find(20*log10(squeeze(mag_area))<=tau_gain, 1, 'first'))
-    tau_num_nep = 1/freq_nep(find(20*log10(squeeze(mag_nep))<=tau_gain, 1, 'first'))
+    tau_gain = w_0-3;
+    tau_num_area = 1/(2*pi*freq_area(find(20*log10(squeeze(mag_area))<=tau_gain, 1, 'first')));
+    tau_num_nep = 1/(2*pi*freq_nep(find(20*log10(squeeze(mag_nep))<=tau_gain, 1, 'first')));
+
+    freq_canto_hz = freq_area(find(20*log10(squeeze(mag_area))<=tau_gain, 1, 'first'));
+    lower_lim_freq_hz = 0.1*freq_canto_hz;
+    upper_lim_freq_hz = 10*freq_canto_hz;
+   
+    logspace(log10(lower_lim_freq_hz), log10(upper_lim_freq_hz),10)
+    % fazer a aquisição de dados das frequências do logspace + 0.1Hz
