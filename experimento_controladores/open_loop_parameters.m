bc4afccdc9996b3564ab4c4a44866e6842bdbf40
Guilherme Matera <guimatera@gmail.com>
2024-11-13 12:26:15 -0300

    feature: Criando script de parâmetros, cálculo do erro permanente e tempo de acomodação.

---
+++ experimento_controladores/open_loop_parameters.m
@@ -0,0 +1,7 @@
+function [Ka, Kt, K_barra, Kp, tau] = open_loop_parameters()
+
+ [Ka, ~, ~] = get_Ka('dados_lin.csv');
+ [Kt, ~, ~] = get_Kt('dados_lin.csv');
+ [tau] = get_tau_area('dados.csv');
+ Kp = 1/(Ka*Kt);
+ K_barra = 1; % Precisamos encontrar a fórmula
