cf5b08f8e5f671376b741664d755e01c3f0a04e9
Guilherme Matera <guimatera@gmail.com>
2024-11-18 11:28:46 -0300

    bugfix: Conserto de parâmetros do controlador PI.

--- experimento_controladores/closed_loop_PI/closed_loop_PI_parameters.m
+++ experimento_controladores/closed_loop_PI/closed_loop_PI_parameters.m
@@ -4,5 +4,5 @@ function [Ka, Kt, K_barra, Kp, Ti, tau] = closed_loop_PI_parameters()
  [Kt, ~, ~] = get_Kt('dados_lin.csv');
  [tau] = get_tau_area('dados.csv');
  K_barra = 1; 
- Kp = 1; % Rever contas
- Ti = 2/(1+(Kp*Ka*Kt));  % Rever contas
+ Kp = 1/(Ka*Kt); 
+ Ti = 1/(tau+12);  
