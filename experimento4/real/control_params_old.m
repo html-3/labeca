99f48536eac9f7b4be269c11e322b74eac4e5900
Henrique Marques <77990915+html-3@users.noreply.github.com>
2024-12-18 16:15:11 -0300

    Adicao de Kr

--- experimento4/real/control_params_old.m
+++ experimento4/real/control_params_old.m
@@ -1,4 +1,4 @@
-function [Kp_old, Ti_old, Kd1, Kd2, Ko, Ki, Ka, Kt, tau] = control_params_old()
+function [Kp_old, Ti_old, Kd1, Kd2, Ko, Ki, Ka, Kt, Kr_old, tau] = control_params_old()
     Kd1 = 0.0005399;
     Kd2 = 1232;
     Ko = 5;
@@ -6,6 +6,7 @@ function [Kp_old, Ti_old, Kd1, Kd2, Ko, Ki, Ka, Kt, tau] = control_params_old()
     Ka = 9.1061;
     Kt = 0.1530;
     tau = 0.0896;
+    Kr_old = 1.39;
     
     Kp_old = 1 / (Ka * Kt);
     z = tau + 12;
